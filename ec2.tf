999143fc8127d3ceeb45e34ee8803ce90509434f
Saishankar <82284490+jyothiadevops@users.noreply.github.com>
2023-12-13 17:40:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,29 @@
-resource "aws_instance" "r100c96" {
-  ami               = "ami-05a5f6298acdb05b6"
-  subnet_id         = "subnet-0b2d78e0c0de9aafa"
-  security_groups   = ["sg-0a349922203ab7abd"]
-  instance_type     = "t2.micro"
-  key_name          = "jenkins"
-  associate_public_ip_address = true
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami}"
+  count = "${var.ec2_instance_count}"
+  instance_type = "${var.instance_type}"
+  availability_zone = var.azs[count.index]
+  vpc_security_group_ids = "${var.security_groups}"
+  subnet_id             = var.subnet_ids[count.index]
+  key_name                 = "${var.key_name}"
   user_data         = file("userdata.sh")
   tags = {
-    Name = "Terraform-diff-linux"
+    Name = "${var.hostname}"
+    SCTaskNo ="${var.SCTaskNo}"
+    Owner = "${var.owner_name}"
+    support_group = "${var.support_group}"
+    Application = "${var.Application}"
+    BU = "${var.BU}"
+    Maintinance_Window = "${var.Maintinance_Window}"
+    Environment = "${var.Environment}"
+    Automation = "${var.Automation}"
+    Backup = "${var.Backup}"
+    
   }
 
 
     provisioner "local-exec" {
-    command = "echo ${aws_instance.r100c96.public_ip} > inventory"
+    command = "echo ${aws_instance.ec2.public_ips} > inventory"
   } 
 
 
@@ -28,9 +39,5 @@ resource "aws_instance" "r100c96" {
 }
 
 output "ip" {
-  value = aws_instance.r100c96.public_ip
-}
-
-output "publicName" {
-  value = aws_instance.r100c96.public_dns
+  value = aws_instance.ec2.public_ips
 }
